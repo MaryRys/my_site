cbe29f4509fb4d56b1158c62e0e56008e6aca8e6
Mary Rys <rys.mary@gmail.com>
2019-08-22 14:04:47 -0500

    routing to pages

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,15 +1,37 @@
 import React from 'react';
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from '../Home/Home';
+import connection from '../../helpers/connection';
+import {
+  BrowserRouter, Route, Switch,
+} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>App</h1>
-      <Home />
-    </div>
-  );
-}
+import Home from '../Home/Home';
+import Projects from '../Projects/Projects';
+
+class App extends React.Component {
+  componentDidMount() {
+    connection();
+  }
+
+  render() {
+
+    return (
+      <div>
+        <BrowserRouter>
+            <React.Fragment>
+                <div className="row">
+                  <Switch>
+                    <Route path='/' exact component={Home} />
+                    <Route path='/home' component={Home} />
+                    <Route path='/projects' component={Projects} />
+                  </Switch>
+                </div>
+            </React.Fragment>
+          </BrowserRouter>
+      </div>
+    )
+    }
+};
 
 export default App;
