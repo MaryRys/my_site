b91b944ab14524acc1246b5f8b16595ec28d85a9
Mary Rys <rys.mary@gmail.com>
2019-08-29 16:23:06 -0500

    added image to homepage

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { Button } from 'reactstrap'
+import myImg from '../../images/me.jpg';
+import './Home.scss';
+
 
 class Home extends React.Component {
   render() {
     return(
       <div className="homeContainer container">
         <h1>Homepage</h1>
-        <Button className="btn btn-info">Button</Button>
+        <div className="myImg">
+          <img src={myImg} alt="headshot"></img>
+        </div>
+
       </div>
     )
   }
